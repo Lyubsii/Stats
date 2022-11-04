1f9aa7c9753e5b674184ff5c733b4fac3f6ea16c
Lyubsii <gordyuhina_lyuba@mail.ru>
2022-11-04 14:33:31 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/services/StatsService.java
@@ -0,0 +1,67 @@
+package ru.netology.stats.services;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long result = 0;
+        for (long sale : sales) {
+            result += sale;
+        }
+        return result;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / 12;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        long maxSale = sales[0];
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= maxSale) {
+                maxSale = sales[i];
+                maxMonth = i;
+            }
+        }
+        return maxMonth;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        long minSale = sales[0];
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= minSale) {
+                minSale = sales[i];
+                minMonth = i;
+            }
+        }
+        return minMonth;
+    }
+
+    public long lowAverage(long[] sales) {
+        long averageSales = average(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < averageSales) {
+                counter ++;
+            }
+        }
+        return counter;
+
+    }
+
+    public long highAverage(long[] sales) {
+        long averageSales = average(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > averageSales) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
+
+    }
